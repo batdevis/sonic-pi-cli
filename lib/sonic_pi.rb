fd4e207dc03715c5707c54075a42549a9b92e371
batdevis <batdevis@gmail.com>
2015-10-19 22:38:34 +0200

    buffer method

--- lib/sonic_pi.rb
+++ lib/sonic_pi.rb
@@ -6,12 +6,17 @@ require 'securerandom'
 class SonicPi
   RUN_COMMAND = "/run-code"
   STOP_COMMAND = "/stop-all-jobs"
+  BUFFER_COMMAND = "/save-and-run-buffer"
   SERVER = 'localhost'
   PORT = 4557
   GUI_ID = 'SONIC_PI_CLI'
 
   def run(command)
     client.send(run_command(command))
+  end
+  
+  def buffer(command)
+    client.send(buffer_command(command))
   end
 
   def stop
@@ -36,6 +41,13 @@ class SonicPi
 
   def run_command(command)
     OSC::Message.new(RUN_COMMAND, GUI_ID, "#{command}")
+  end
+  
+  #https://github.com/samaaron/sonic-pi/wiki/Sonic-Pi-Internals----GUI-Ruby-API#save-and-run-buffer-filename-code-workspace
+  def buffer_command(command)
+    filename = "file_#{GUI_ID}"
+    workspace = "workspace_zero"
+    OSC::Message.new(BUFFER_COMMAND, GUI_ID, filename, "#{command}", workspace)
   end
 
   def stop_command
